ffb797d74c1ea866ec51105049899e1f27950119
William Hogan <hoganwr@WilliamacStudio.attlocal.net>
2022-11-19 16:42:26 -0500

    initial commit of rt2. I have a Rui class and some code that tests it working. Note the repo uses pipenv

---
+++ rui/Rui.py
@@ -0,0 +1,19 @@
+from uuid6 import uuid7
+
+class Rui:
+	def __init__(self, a_or_r):
+		self.uuid = uuid7()
+		if (a_or_r == 'A' or a_or_r == 'R'):
+			self.a_or_r = a_or_r
+		else:
+			raise Exception("a_or_r must be set to A (assigned) or R (reserved)")
+
+	def is_assigned(self):
+		return (self.a_or_r == 'A')
+
+	def is_reserved(self): 
+		return (self.a_or_r == 'R')
+
+	def get_uuid(self):
+		return self.uuid
+
---
+++ test/test_rui.py
@@ -0,0 +1,20 @@
+import sys
+  
+# append the path of the
+# parent directory
+sys.path.append(".")
+
+from rui import Rui
+
+
+def print_info(rui):
+	print(rui)
+	print("assigned? " + str(rui.is_assigned()))
+	print("reserved? " + str(rui.is_reserved()))
+	print("uuid: " + str(rui.get_uuid()))
+
+x = Rui.Rui('A')
+y = Rui.Rui('R')
+
+print_info(x)
+print_info(y)
