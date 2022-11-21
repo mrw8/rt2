f1f521eb9d12ace14c8ce7718a65946763d6616e
William Hogan <hoganwr@gmail.com>
2022-11-21 17:11:22 -0500

    adding a little class inheritance that will be useful later

--- rtt/atuple.py
+++ rtt/atuple.py
@@ -9,13 +9,17 @@ sys.path.append(".")
 
 from ids_codes import Rui
 
-class Atuple:
-	"""Referent Tracking assignment tuple that registers assignment of an RUI to a PoR"""
-	def __init__(self, ruip=None, ruia=None, ruit=None, unique=None, ar=None, t=None):
+class RtTuple:
+	def __init__(self, ruit):
 		if ruit is None:
 			self.ruit = Rui.Rui('A')
 		else:	
 			self.ruit = ruit
+
+class Atuple(RtTuple):
+	"""Referent Tracking assignment tuple that registers assignment of an RUI to a PoR"""
+	def __init__(self, ruip=None, ruia=None, ruit=None, unique=None, ar=None, t=None):
+		super().__init__(ruit)
 
 		if ar is None:
 			self.ar = 'A'
