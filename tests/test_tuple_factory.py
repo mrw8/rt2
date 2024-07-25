43ce99b58f451bcb909fceae44d51184db1b9b08
Mitchell Ward <miward@mcw.edu>
2024-07-25 11:19:41 -0500

    Removed rT and added support back for datetimes in TempRefs

--- tests/test_tuple_factory.py
+++ tests/test_tuple_factory.py
@@ -10,7 +10,8 @@ from rt_core_v2.rttuple import (
     NtoLackRTuple,
     TupleType,
     PorType,
-    RuiStatus
+    RuiStatus,
+    TupleComponents
 )
 
 from rt_core_v2.metadata import TupleEventType, RtChangeReason
@@ -39,8 +40,11 @@ code = "code insert"
 inst = "instance of"
 data = "data insert"
 
+
+#TODO Fill out tests for tuple factory usage
 def test_atuple_factory():
     a = ATuple(ruit, ruia, ruip, ar, unique, time_1)
+    # a_args = {TupleComponents.ruit:ruit, TupleComponents.ruit:ruia, TupleComponents.ruit:ruip, TupleComponents.ruit:ar, TupleComponents.ruit:unique, TupleComponents.ruit:time_1}
     # a_fac, d_fac = insert_rttuple({:ruit, :ruia, :ruip, :ar, :unique, :time_1}, TupleType.ATuple)
 # def test_dtuple_factory():
 
