3b6e44d619755746d189542a1e7a2ee2574f0b77
Mitchell Ward <mrwardtexas@gmail.com>
2024-10-01 10:38:14 -0500

    Removed todo

--- src/rt_core_v2/rttuple.py
+++ src/rt_core_v2/rttuple.py
@@ -291,7 +291,6 @@ class NtoCTuple(RtTuple):
     ruin: Rui = field(default_factory=Rui)
     code: str = ""
     tr: TempRef = field(default_factory=TempRef)
-    # TODO Make creating a tempref create an underlying time instance
 
 
 # We use NtoDE instead of NtoI, and we use an instance for the identifying descriptor
